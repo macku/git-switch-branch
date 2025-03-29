ae4eb02a394d24c18f882ca734291c1cc86d0eec
Maciej Adamczak <me@maciejadamczak.net>
2025-03-29 21:59:03 +1100

    chore: disable warnings

--- cli/git-builds.ts
+++ cli/git-builds.ts
@@ -1,3 +1,3 @@
-#!/usr/bin/env node --no-warnings --experimental-strip-types
+#!/usr/bin/env node --disable-warning=ExperimentalWarning --experimental-strip-types
 
 import '../src/cmd/git-builds.ts';
